2fcdaf93e6db4935488675249d02b6111dd12f3b
Jonathan Ramirez <jonksff@gmail.com>
2023-06-11 16:25:55 -0400

    DONE: navbar styling

--- src/components/UI/NavBar.jsx
+++ src/components/UI/NavBar.jsx
@@ -2,6 +2,11 @@ import classes from '../modules/navbar.module.css';
 import LogoIcon from '../../assets/icons/linkedin-logo-icon.png';
 import { Link } from 'react-router-dom';
 import * as helper from '../../helpers/config';
+import { AiOutlineSearch, AiOutlineHome, AiOutlineBell } from 'react-icons/ai';
+import { BsPeople, BsBriefcase, BsChatDots } from 'react-icons/bs';
+import { TbGridDots } from 'react-icons/tb';
+import { ImNewspaper } from 'react-icons/im';
+import UserIcon from '../../assets/icons/user.png';
 
 const NavBar = () => {
 	return (
@@ -17,16 +22,81 @@ const NavBar = () => {
 			</div>
 			<div>
 				<ul className={classes.links}>
-					<li className={classes['link-item']}>Home</li>
-					<li className={classes['link-item']}>About</li>
+					<li className={classes.link}>
+						<a href='#'>
+							<AiOutlineSearch
+								size={30}
+								className={classes.icon}
+							/>
+						</a>
+					</li>
+					<li className={classes.link}>
+						<a href='#'>
+							<AiOutlineHome
+								size={30}
+								className={classes.icon}
+							/>
+						</a>
+					</li>
+					<li className={classes.link}>
+						<a href='#'>
+							<BsPeople
+								size={30}
+								className={classes.icon}
+							/>
+						</a>
+					</li>
+					<li className={classes.link}>
+						<a href='#'>
+							<BsBriefcase
+								size={30}
+								className={classes.icon}
+							/>
+						</a>
+					</li>
+					<li className={classes.link}>
+						<a href='#'>
+							<BsChatDots
+								size={30}
+								className={classes.icon}
+							/>
+						</a>
+					</li>
+					<li className={classes.link}>
+						<a href='#'>
+							<TbGridDots
+								size={30}
+								className={classes.icon}
+							/>
+						</a>
+					</li>
+
+					<li className={classes.link}>
+						<a href='#'>
+							<AiOutlineBell
+								size={30}
+								className={classes.icon}
+							/>
+						</a>
+					</li>
+					<li className={classes.link}>
+						<a href='#'>
+							<ImNewspaper
+								size={30}
+								className={classes.icon}
+							/>
+						</a>
+					</li>
 				</ul>
 			</div>
 			<div>
-				<img
-					src='#'
-					alt=''
-				/>
-				<p>Log out</p>
+				<a href='#'>
+					<img
+						className={classes['profile-img']}
+						src={UserIcon}
+						alt=''
+					/>
+				</a>
 			</div>
 		</nav>
 	);
