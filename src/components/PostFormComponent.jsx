ef8bae83e97590b9f4b05dc9094b1d6268b36c9d
Jonathan Ramirez <jonksff@gmail.com>
2023-06-13 06:11:15 -0400

    FIXED: updating state as we create objects

--- src/components/PostFormComponent.jsx
+++ src/components/PostFormComponent.jsx
@@ -3,21 +3,24 @@ import classes from './modules/home.module.css';
 import { useState } from 'react';
 import Modal from './UI/Modal';
 import { createPost } from '../api/FirestoreAPI';
-
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { postsActions } from '../store/posts-slice';
 
 const PostFormComponent = () => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [inputValue, setInputValue] = useState('');
 	const [isValid, setIsValid] = useState(false);
 	const user = useSelector((state) => state.user);
+	const dispatch = useDispatch();
 
 	const showModal = () => {
 		setIsModalOpen(true);
 	};
 
 	const handleOk = async () => {
-		createPost({ user, content: inputValue });
+		const post = { user, content: inputValue };
+		await createPost(post);
+		dispatch(postsActions.addNewPost({ post }));
 		setInputValue('');
 		setIsModalOpen(false);
 	};
