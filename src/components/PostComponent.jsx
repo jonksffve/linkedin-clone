11fab7e06598ab932a7d19f29b5d02880b3707be
Jonathan Ramirez <jonksff@gmail.com>
2023-06-15 17:53:33 -0400

    Posts redirecting to user profile

--- src/components/PostComponent.jsx
+++ src/components/PostComponent.jsx
@@ -1,17 +1,22 @@
-import { Fragment } from 'react';
 import classes from './modules/card.module.css';
+import { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 
 const PostComponent = ({ post }) => {
 	return (
 		<Fragment>
 			<div className={classes.header}>
-				<img
-					className={`${classes['profile-img']} ${classes.small}`}
-					src={post.user.photo}
-					alt=''
-				/>
+				<Link to={`/profile/${post.user.id}`}>
+					<img
+						className={`${classes['profile-img']} ${classes.small}`}
+						src={post.user.photo}
+						alt=''
+					/>
+				</Link>
 				<div className={classes.subheader}>
-					<h3>{post.user.name}</h3>
+					<Link to={`/profile/${post.user.id}`}>
+						<h3>{post.user.name}</h3>
+					</Link>
 					<small>Description</small>
 				</div>
 			</div>
