0d3fb8591ae77c75963ec50858e4bad170a0f4d6
Jonathan Ramirez <jonksff@gmail.com>
2023-06-16 18:45:42 -0400

    DONE menu user styling

--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -1,7 +1,9 @@
 import classes from '../modules/card.module.css';
 
-const Card = ({ children }) => {
-	return <div className={classes.card}>{children}</div>;
+const Card = ({ children, customClass = '' }) => {
+	let componentClasses = `${classes.card} ${customClass}`;
+
+	return <div className={componentClasses}>{children}</div>;
 };
 
 export default Card;
