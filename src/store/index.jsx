a4f4102e8d4eaaa62c4eeac9402e72d438b14e7e
Jonathan Ramirez <jonksff@gmail.com>
2023-06-17 02:14:11 -0400

    Deleted posts slice redux

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user-slice';
-import postsReducer from './posts-slice';
 
 export const store = configureStore({
 	reducer: {
 		user: userReducer,
-		posts: postsReducer,
 	},
 });
