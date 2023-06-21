c8566a3cd28557945211c0bee4d4765fc98e13a3
Jonathan Ramirez <jonksff@gmail.com>
2023-06-21 07:26:05 -0400

    styled connection component

--- src/components/ConnectionsComponent.jsx
+++ src/components/ConnectionsComponent.jsx
@@ -1,8 +1,32 @@
+import Card from './UI/Card';
+import classes from './modules/card.module.css';
+import ImgBanner from '../assets/images/banner.jfif';
+import ImgPhoto from '../assets/images/profile.jpg';
+
 const ConnectionsComponent = () => {
 	return (
-		<div>
-			<h2>Hello</h2>
-		</div>
+		<Card customClass={classes.connections}>
+			<h2>Follow more professionals!</h2>
+			<Card customClass={classes['card-connection']}>
+				<div className={classes.header}>
+					<img
+						className={classes.banner}
+						src={ImgBanner}
+						alt=''
+					/>
+					<img
+						className={`${classes['profile-img']} ${classes.medium}`}
+						src={ImgPhoto}
+						alt=''
+					/>
+				</div>
+				<div className={classes.subheader}>
+					<h3>Name</h3>
+					<small>Headline</small>
+				</div>
+				<button>Connect</button>
+			</Card>
+		</Card>
 	);
 };
 
